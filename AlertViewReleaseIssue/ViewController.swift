085c19e44a1ee7161ffce898f458edec792e4c99
Min <qwer810520@gmail.com>
2020-09-15 10:18:09 +0800

    Initialization

---
+++ AlertViewReleaseIssue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AlertViewReleaseIssue
+//
+//  Created by Min on 2020/9/15.
+//  Copyright © 2020 Min. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+
+}
+
